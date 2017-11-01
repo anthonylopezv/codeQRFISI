2dc3f3c8dba9a7bc0f8e3d777d03c26b0134eff9
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2017-11-01 11:44:30 -0500

    Cambios hechos en la clase de calidad de software el dia 31 de octubre del presente año

---
+++ app/src/main/java/codeqr/unmsm/com/codeqr_fisi/login.java
@@ -0,0 +1,27 @@
+package codeqr.unmsm.com.codeqr_fisi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    private Button pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        pass = (Button) findViewById(R.id.pass);
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
