217141cda38f28005f0e6cccced9494e6dc8ec4a
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2017-11-02 01:28:27 -0500

    Modificando el diseño del login y colores

--- app/src/main/java/codeqr/unmsm/com/codeqr_fisi/login.java
+++ app/src/main/java/codeqr/unmsm/com/codeqr_fisi/login.java
@@ -1,16 +1,16 @@
 package codeqr.unmsm.com.codeqr_fisi;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
 
 public class login extends AppCompatActivity {
 
     private Button pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
